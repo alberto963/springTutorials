85dc7655e2b70690ef95d598fa152e79537c08c1
apetazzi <petazzi63@gmail.com>
2016-04-16 10:15:42 +0200

    added bean config java based example

---
+++ springWebContextServlet/src/main/java/com/demo/AppConfig.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.demo.services.Service_0;
+import com.demo.services.Service_0_Impl;
+
+/**
+ * The AppConfig class would be equivalent to the following Spring <beans/> XML:
+ * <beans> <bean id="Service_0_Impl" class="com.demo.services.Service_0_Impl"/>
+ * </beans>
+ */
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public Service_0 service_0() {
+		return new Service_0_Impl();
+	}
+}
