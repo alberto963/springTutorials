bad65f7117826627c6f728a0e9996ba50c7e375f
Alberto <petazzi63@gmail.com>
2016-04-25 09:36:31 +0200

    changed traces

--- springWebContextServletExample/src/main/java/org/baeldung/context/services/FileLogService.java
+++ springWebContextServletExample/src/main/java/org/baeldung/context/services/FileLogService.java
@@ -2,7 +2,7 @@ package org.baeldung.context.services;
 
 public class FileLogService implements LogService {
     public String logAccess(String logInfo) {
-        String ret = "logging access on file " + logInfo;
+        String ret = "logging access on FILE " + logInfo;
         System.out.println(ret);
         return ret;
     }
--- springWebContextServletExample/src/main/java/org/baeldung/context/services/SocketLogService.java
+++ springWebContextServletExample/src/main/java/org/baeldung/context/services/SocketLogService.java
@@ -2,7 +2,7 @@ package org.baeldung.context.services;
 
 public class SocketLogService implements LogService {
     public String logAccess(String logInfo) {
-        String ret = "logging access on socket " + logInfo;
+        String ret = "logging access on SOCKET " + logInfo;
         System.out.println(ret);
         return ret;
     }
