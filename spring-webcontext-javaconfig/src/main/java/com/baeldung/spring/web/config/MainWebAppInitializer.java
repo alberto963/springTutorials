2d09e2432e992beb8613c2d6ee232b2d34ff9adb
Alberto <petazzi63@gmail.com>
2016-06-28 16:03:30 +0200

    Initial

---
+++ spring-webcontext-javaconfig/src/main/java/com/baeldung/spring/web/config/MainWebAppInitializer.java
@@ -0,0 +1,38 @@
+package com.baeldung.spring.web.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MainWebAppInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext sc) throws ServletException {
+		
+		// Create the 'root' Spring application context
+		final AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.setConfigLocation("com.baeldung.spring.web.config.root");
+
+		// Manages the lifecycle of the root application context
+		sc.addListener(new ContextLoaderListener(rootContext));
+
+		AnnotationConfigWebApplicationContext mainBuildingContext = new AnnotationConfigWebApplicationContext();
+		mainBuildingContext.setConfigLocation("com.baeldung.spring.web.config.mainBuilding");
+		final ServletRegistration.Dynamic mainBuildingServlet = sc.addServlet("MainBuilding",
+				new DispatcherServlet(mainBuildingContext));
+		mainBuildingServlet.setLoadOnStartup(1);
+		mainBuildingServlet.addMapping("/MainBuilding/*");
+		
+		AnnotationConfigWebApplicationContext secondaryBuildingContext = new AnnotationConfigWebApplicationContext();
+		secondaryBuildingContext.setConfigLocation("com.baeldung.spring.web.config.secondaryBuilding");
+		final ServletRegistration.Dynamic secondaryBuildingServlet = sc.addServlet("SecondaryBuilding",
+				new DispatcherServlet(secondaryBuildingContext));
+		secondaryBuildingServlet.setLoadOnStartup(1);
+		secondaryBuildingServlet.addMapping("/SecondaryBuilding/*");
+	}
+
+}
