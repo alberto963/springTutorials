47a499744f0bb7e84537a09b911c30b9f20188e6
apetazzi <petazzi63@gmail.com>
2017-09-14 16:43:38 +0200

    Initial version my_04 (from http://websystique.com)

---
+++ my_04_SpringBootCRUDApplication/ReadMe.md
@@ -0,0 +1,5 @@
+**Ref:**
+http://websystique.com/spring-boot/spring-boot-angularjs-spring-data-jpa-crud-app-example/
+
+**Urls:**
+http://localhost:8080/SpringBootCRUDApp
---
+++ my_04_SpringBootCRUDApplication/src/main/java/com/websystique/springboot/service/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.websystique.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.websystique.springboot.model.User;
+import com.websystique.springboot.repositories.UserRepository;
+
+@Service("userService")
+@Transactional
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User findById(Long id) {
+		return userRepository.findOne(id);
+	}
+
+	@Override
+	public User findByName(String name) {
+		return userRepository.findByName(name);
+	}
+
+	@Override
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
+
+	@Override
+	public void updateUser(User user) {
+		saveUser(user);
+	}
+
+	@Override
+	public void deleteUserById(Long id) {
+		userRepository.delete(id);
+	}
+
+	@Override
+	public void deleteAllUsers() {
+		userRepository.deleteAll();
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public boolean isUserExist(User user) {
+		return findByName(user.getName()) != null;
+	}
+
+}
