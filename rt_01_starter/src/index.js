d8430461f5ab93077d668326977ce4b0fecdd684
apetazzi <petazzi63@gmail.com>
2018-04-09 16:31:12 +0200

    rt_01: tutorial improvement #3 (Rewrite Board to use two loops to make the squares instead of hardcoding them.) First attempt.

--- rt_01_starter/src/index.js
+++ rt_01_starter/src/index.js
@@ -13,42 +13,32 @@ function Square(props) {
   )
 }
 
-// eslint-disable-next-line 
-function SquareOrig(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      <div>
-          <span className="num">{props.num}</span>      
-          <span>{props.value}</span>      
-        </div>
-    </button>
-  )
-}
-class Board extends React.Component {
-    renderSquare(i) {
-    return <Square num={i} 
-      value={this.props.squares[i]} 
-      onClick={() => this.props.onClick(i)} />
+class Row extends React.Component {
+ 
+  renderSquare(i) {
+  return <Square num={i} 
+    value={this.props.squares[i]} 
+    onClick={() => this.props.onClick(i)} />
   }
+  render () {
+    return (
+      <div className="board-row">
+            {this.renderSquare(this.props.r * 3)}
+            {this.renderSquare(this.props.r * 3 + 1)}
+            {this.renderSquare(this.props.r * 3 + 2)}
+      </div>
+    )
+  }
+}
 
+class Board extends React.Component {
+ 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <Row r="0" squares={this.props.squares} onClick={this.props.onClick} />
+        <Row r="1" squares={this.props.squares} onClick={this.props.onClick} />
+        <Row r="2" squares={this.props.squares} onClick={this.props.onClick} />
       </div>
     )
   }
