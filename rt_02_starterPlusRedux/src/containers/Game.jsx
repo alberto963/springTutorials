d370b41f5bd39307faf009bb76b3feb14273dc98
apetazzi <petazzi63@gmail.com>
2018-05-16 17:35:12 +0200

    rt_02: tutorial: 2.2) tring to show properly game (board).

--- rt_02_starterPlusRedux/src/containers/Game.jsx
+++ rt_02_starterPlusRedux/src/containers/Game.jsx
@@ -6,27 +6,23 @@ import Board from './../components/Board'
 import { reverse } from "../actions/index"
 import { click } from "../actions/index"
 
+const mapStateToProps = (state, ownProps) => {
+  return { 
+    history: state.click.squares,
+    stepNumber: state.stepNumber,
+    ascending: state.ascending,
+    xIsNext: state.xIsNext,
+    winSequence: state.winSequence,
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
-    reverse: () => dispatch(reverse()),
-    click: (i) => dispatch(click(i)),
-  };
-};
-class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-        i : null,
-      }],
-      ascending: true,   // Added for improvement #4 (Add a toggle button that lets you sort the moves in either ascending or descending order.)
-      xIsNext: true,
-      stepNumber: 0,
-      winSequence: null,
-    }
+    reverse,
+    click,
   }
+}
+class Game extends React.Component {   
   handleReverse() {
     this.props.reverse() // Relevant Redux part!!
   }
@@ -35,15 +31,13 @@ class Game extends React.Component {
     this.props.click(i) // Relevant Redux part!!
   }
   render() {
-    const history = this.state.history;
-    const stepNumber = this.state.stepNumber
     
-    const moves = history.map((step, move) => {
+    const moves = this.props.history.map((step, move) => {
       // Added for improvement #4 (Add a toggle button that lets you sort the moves in either ascending or descending order.)
-      const pos = this.state.ascending ? move : history.length - move - 1 
+      const pos = this.props.ascending ? move : this.props.history.length - move - 1 
       const desc = pos ? 'Go to move #' + pos :'Go to game start'
       const location = pos ? 'Move Location col=' + (step.i % 3) + ', row=' + Math.floor(step.i/3) + ', player=' + step.squares[step.i] : ''
-      const currentmove = stepNumber === move ? 'true' : 'false' // Added for improvement #2 (Bold the currently selected item in the move list.)
+      const currentmove = this.props.stepNumber === move ? 'true' : 'false' // Added for improvement #2 (Bold the currently selected item in the move list.)
 
       return (
         <li key={move}>
@@ -54,7 +48,7 @@ class Game extends React.Component {
     });
 
     // Changed for improvement #5 (When someone wins, highlight the three squares that caused the win.)
-    const current = history[stepNumber];
+    const current = this.props.history[this.props.stepNumber];
     const winSequence = calculateWinSequence(current.squares)
 
     let status;
@@ -62,14 +56,14 @@ class Game extends React.Component {
       status = 'Winner: ' + (!this.state.xIsNext ? 'X' : 'O')
     } else {
       // Changed for improvement #6 (When no one wins, display a message about the result being a draw.)
-      const draw = this.state.ascending ? stepNumber === 9 : stepNumber === 0
-      status = draw ? 'Draw!' : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const draw = this.props.ascending ? this.props.stepNumber === 9 : this.props.stepNumber === 0
+      status = draw ? 'Draw!' : 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
     }
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} winSequence={this.state.winSequence} />
+          <Board squares={current.squares} onClick={this.handleClick} winSequence={this.props.winSequence} />
         </div>
         <div className="game-info">
           <div>{status}</div>
@@ -103,4 +97,4 @@ function calculateWinSequence(squares) {
 }
 
 // export default Game
-export default connect(null, mapDispatchToProps)(Game)
+export default connect(mapStateToProps, mapDispatchToProps)(Game)
