4288a9197d7ed219e3c00d30401c5df4fd58c455
apetazzi <petazzi63@gmail.com>
2018-05-17 17:46:45 +0200

    rt_02: tutorial: trying to implement Square correctly

--- rt_02_starterPlusRedux/src/components/Square.jsx
+++ rt_02_starterPlusRedux/src/components/Square.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import { connect } from "react-redux"
+
 import './../index.css'
+import { click, } from "../actions/index"
 
 const mapStateToProps = (state) => {
   return { 
@@ -8,14 +11,14 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = {
-  onClick,
+  click,
 }
 
-function Square(props) {
+function WrappedSquare(props) {
   let winner = props.winSequence && props.winSequence.includes(props.num)
   winner = winner ? winner.toString() : 'false'
   return (
-    <button className="square-row" onClick={props.onClick} >
+    <button className="square-row" onClick={props.click} >
       <div>
         <span className="num">{props.num}</span>      
         <span className="square" winner={winner}>{props.value}</span>
@@ -23,5 +26,7 @@ function Square(props) {
     </button>
   )
 }
+
+const Square = connect(mapStateToProps, mapDispatchToProps)(WrappedSquare)
 
 export default Square
