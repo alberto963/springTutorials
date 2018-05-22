a525b92106b8cefdfd1271ff4a580dea5c5b1cde
apetazzi <petazzi63@gmail.com>
2018-05-22 13:49:27 +0200

    rt_02: tutorial:more simplification and actual better use of redux 3 (converted react classes om functions)

--- rt_02_starterPlusRedux/src/components/Board.jsx
+++ rt_02_starterPlusRedux/src/components/Board.jsx
@@ -1,22 +1,12 @@
-import React from 'react'
-import './../index.css'
+import React from "react";
+import "./../index.css";
 
-import Row from './Row'
-class Board extends React.Component {
-  // Refactored for improvement #3: Rewrite Board to use two loops to make the squares instead of hardcoding them..
-   render() {
- 
-    // let self=this
-     const rows = Array(3).fill(null).map((row, r) => {
-       return (<Row r={r} click={this.props.click} />)
-     })
- 
-     return (
-       <div>
-         {rows}
-       </div>
-     )
-   }
- }
- 
- export default Board
+import Row from "./Row";
+
+export default function Board() {
+  const rows = Array(3).fill(null).map((row, r) => {
+      return <Row r={r} click={this.props.click} />;
+    });
+
+  return <div>{rows}</div>;
+}
--- rt_02_starterPlusRedux/src/components/Row.js
+++ rt_02_starterPlusRedux/src/components/Row.js
@@ -1,25 +1,14 @@
-import React from 'react'
-import './../index.css'
+import React from "react"
+import "./../index.css"
 
-import Square from './Square'
+import Square from "./Square"
 
-class Row extends React.Component {
-  // Refactored for improvement #3: Rewrite Board to use two loops to make the squares instead of hardcoding them..
+export default function Row(props) {
+  const r = this.props.r * 3
 
-  render () {
-
-    const r = this.props.r * 3
-
-    const squares = Array(3).fill(null).map((square, i) => {
-      return (<Square num={r + i} click={() => this.props.click(r + i)} />)
+  const squares = Array(3).fill(null).map((square, i) => {
+      return <Square num={r + i} click={() => this.props.click(r + i)} />
     })
 
-    return (
-      <div className="board-row">
-        {squares}
-      </div>
-    )
-  }
+  return <div className="board-row">{squares}</div>
 }
-
-export default Row
