b8f93a0bcae6921594f35b5be3455175469a3c61
apetazzi <petazzi63@gmail.com>
2018-05-29 11:14:39 +0200

    rt_04: initial version (display configurable charts)experimenting with data

--- rt_04_victoryJson/src/index.js
+++ rt_04_victoryJson/src/index.js
@@ -25,31 +25,31 @@ const struct = [
 ]
 
 /*
- * Build an algorithm that builds the following array object on user request
+ * Write an algorithm that builds the following array object on user request
  */
 const pieData_F1 = [
-  {x: 1, y: 3, label: 'F1-1'},
-  {x: 2, y: 3, label: 'F1-2'},
-  {x: 3, y: 3, label: 'F1-3'},
+  {f1: 1, y: 3, label: 'F1-1'},
+  {f1: 2, y: 3, label: 'F1-2'},
+  {f1: 3, y: 3, label: 'F1-3'},
 ]
 
-const pieData_F2 = [
-  {x: 4, y: 4, label: 'F2-T'},
-  {x: 5, y: 5, label: 'F2-F'},
+const pieData_F3 = [
+  {f3: true, y: 4, label: 'F3-T'},
+  {f3: false, y: 5, label: 'F3-F'},
 ]
 
 const barData_1 = [
-  {x: 1, y: 3, label: 'F1-1'},
-  {x: 2, y: 4, label: 'F2-T'},
+  {x: 1, y: 3, label: 'F1-1', other: 'O'},
+  {x: 2, y: 4, label: 'F3-T', other: 'O'},
 ]
 
 const barData_2 = [
-  {x: 1, y: 3, label: 'F1-2'},
-  {x: 2, y: 5, label: 'F2-F'},
+  {x: 1, y: 3, label: 'F1-2', other: 'O'},
+  {x: 2, y: 5, label: 'F3-F', other: 'O'},
 ]
 
 const barData_3 = [
-  {x: 1, y: 3, label: 'F1-3'},
+  {x: 1, y: 3, label: 'F1-3', other: 'O'},
 ]
 
 const pieStyle = { labels: { fontSize: 10, fill: 'black'}}
@@ -60,11 +60,11 @@ class JPie extends React.Component {
         <div className='panel-elem'>
           <svg values='' width={200} height={200}>
             <V.VictoryPie standalone={false} width={200} height={200} data={this.props.data} innerRadius={25} labelRadius={50}
+                          x={this.props.field} y="y"
                           style={pieStyle} colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
-                          animate={{duration: 2000, onLoad: { duration: 1000 }}}
-                          />
+                          animate={{duration: 2000, onLoad: { duration: 1000 }}} />
 
-            <V.VictoryLabel textAnchor='middle' style={{ fontSize: 10 }} x={100} y={100} text={this.props.text} />
+            <V.VictoryLabel textAnchor='middle' style={{ fontSize: 10 }} x={100} y={100} text={this.props.title} />
           </svg>
         </div>
     )
@@ -78,23 +78,19 @@ class JBar extends React.Component {
       <div className='panel-elem' >
         <V.VictoryChart domainPadding={10} theme={V.VictoryTheme.material}>
 
-          <V.VictoryAxis tickValues={["F1", "F2",]}/>
+          <V.VictoryAxis tickValues={["F1", "F3",]}/>
           <V.VictoryAxis dependentAxis tickFormat={(x) => (`${x}`)}/>
 
           <V.VictoryStack width={100} height={100} standalone={false} style={barStyle}>
-            <V.VictoryBar data={this.props.data[0]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}} 
-            dataComponent={
-              <V.Bar
-                events={{
-                  onClick: (evt) => alert(`(${evt.clientX}, ${evt.clientY})`)
-                }}
-              />
-            } />
-            <V.VictoryBar data={this.props.data[1]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}} />
-            <V.VictoryBar data={this.props.data[2]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}} />
+            <V.VictoryBar data={this.props.data[0]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}}
+                          dataComponent={ <V.Bar events={{ onClick: (evt) => alert(`(${evt.clientX}, ${evt.clientY})`) }} /> } />
+            <V.VictoryBar data={this.props.data[1]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}}
+                                                    />
+            <V.VictoryBar data={this.props.data[2]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}}
+                                                   />
           </V.VictoryStack>
 
-          <V.VictoryLabel textAnchor='start' style={{ fontSize: 20 }} x={150} y={10} labelPlacement='parallel' text={this.props.text} />
+          <V.VictoryLabel textAnchor='start' style={{ fontSize: 20 }} x={150} y={10} labelPlacement='parallel' text={this.props.title} />
 
         </V.VictoryChart>
       </div>
@@ -107,9 +103,9 @@ class StatsPanel extends React.Component {
     return (
       <div className='panel'>
         <div className='panel-row' >
-          <JPie data={pieData_F1} text='pie1-1' />
-          <JPie data={pieData_F2} text='pie1-2'/>
-          <JBar data={[barData_1, barData_2, barData_3]} text='bar1' />
+          <JPie data={pieData_F1} field={this.props.struct[0].fields[0]} title={this.props.struct[0].title + '-1'} />
+          <JPie data={pieData_F3} field={this.props.struct[0].fields[1]} title={this.props.struct[0].title + '-2'}/>
+          <JBar data={[barData_1, barData_2, barData_3]} title={this.props.struct[1].title} />
         </div>
     </div>
     )
