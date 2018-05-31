183b6c38521ff9ee042b78802f373aadbed02c13
apetazzi <petazzi63@gmail.com>
2018-05-31 11:45:27 +0200

    rt_04: initial version (display configurable charts) fixing styles

--- rt_04_victoryJson/src/index.js
+++ rt_04_victoryJson/src/index.js
@@ -17,11 +17,11 @@ const data = [
 ]
 
 const struct = [
-  { title: 'pie1', type: 'pie', category: ['f1', 'f2', 'f3'] }, 
-  { title: 'bar1', type: 'bar', category: ['f1', 'f3'] },
+  { title: 'pie1', type: 'pie', attributes: ['f1', 'f2', 'f3'] }, 
+  { title: 'bar1', type: 'bar', attributes: ['f1', 'f2', 'f3'] },
 
-  { title: 'pie2', type: 'pie', category: ['f2', 'f3'] }, 
-  { title: 'bar2', type: 'bar', category: ['f2', 'f3'] },
+  { title: 'pie2', type: 'pie', attributes: ['f2', 'f3'] }, 
+  { title: 'bar2', type: 'bar', attributes: ['f2', 'f3'] },
 ]
 
 /*
@@ -63,14 +63,14 @@ class JPie extends React.Component {
                           style={pieStyle} colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
                           animate={{duration: 2000, onLoad: { duration: 1000 }}} />
 
-            <V.VictoryLabel textAnchor='middle' style={{ fontSize: 10 }} x={100} y={100} text={this.props.title} />
+            <V.VictoryLabel textAnchor='middle' style={{ fontSize: 10 }} x={100} y={100} text={this.props.struct.title + "-"+ this.props.title}  />
           </svg>
         </div>
     )
   }
 }
 
-const barStyle = { labels: { fontSize: 20, fill: 'black'}}
+const multiBarStyle = { labels: { fontSize: 20, fill: "black" }}
 class JBar extends React.Component {
   render() {
     return (
@@ -80,7 +80,7 @@ class JBar extends React.Component {
           <V.VictoryAxis tickValues={["F1", "F3",]}/>
           <V.VictoryAxis dependentAxis tickFormat={(x) => (`${x}`)}/>
 
-          <V.VictoryStack width={100} height={100} standalone={false} style={barStyle}>
+          <V.VictoryStack width={100} height={100} standalone={false} style={multiBarStyle}>
             <V.VictoryBar data={this.props.data[0]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}}
                           dataComponent={ <V.Bar events={{ onClick: (evt) => alert(`(${evt.clientX}, ${evt.clientY})`) }} /> } />
             <V.VictoryBar data={this.props.data[1]} labelComponent={<V.VictoryLabel dx={40} dy={30}/>} animate={{duration: 2000, onLoad: { duration: 1000 }}}
@@ -109,10 +109,10 @@ class JBar2 extends React.Component {
       <div className='panel-elem' >
         <V.VictoryChart domainPadding={10} theme={V.VictoryTheme.material}>
 
-          <V.VictoryAxis tickValues={this.props.struct.category}/>
+          <V.VictoryAxis tickValues={this.props.struct.attributes}/>
           <V.VictoryAxis dependentAxis tickFormat={(x) => (`${x}`)}/>
 
-          <V.VictoryStack width={100} height={100} standalone={false} style={barStyle}>
+          <V.VictoryStack width={100} height={100} standalone={false} style={multiBarStyle}>
             {bars}
           </V.VictoryStack>
 
@@ -124,15 +124,27 @@ class JBar2 extends React.Component {
   }
 }
 
-class SingleAttributJBar extends React.Component {
+const barStyle = { labels: { fontSize: 20, fill: "black" },
+                   data: { fill: (d) => d.x <0 ? "#000000" : "#c43a31", stroke: (d) => d.x < 0 ? "#000000" : "#c43a31", fillOpacity: 0.7, strokeWidth: 3 }}
+
+class SingleAttributeJBar extends React.Component {
   render() {
     return (
       <div className='panel-elem' >
         <V.VictoryChart domainPadding={10} theme={V.VictoryTheme.material}>
-          <V.VictoryAxis tickValues={[" 1", " 2", " 3"] } />
-          <V.VictoryAxis dependentAxis tickFormat={(x) => (`${x}`)}/>
-          <V.VictoryBar width={100} height={100} standalone={false} data={this.props.data} />
-          <V.VictoryLabel textAnchor='start' style={{ fontSize: 20 }} x={150} y={10} labelPlacement='parallel' text={this.props.title} />
+          <V.VictoryLabel textAnchor='start' style={{ fontSize: 20 }} x={150} y={10} labelPlacement='parallel' text={this.props.struct.title + "-"+ this.props.title} />
+          <V.VictoryAxis tickValues={Array.from(this.props.data.values.keys())} label="Label"
+  style={{
+    axis: {stroke: "#756f6a"},
+    axisLabel: {fontSize: 20, padding: 30},
+    grid: {stroke: (t) => t > 0.5 ? "red" : "grey"},
+    ticks: {stroke: "grey", size: 5},
+    tickLabels: {fontSize: 15, padding: 5, margin: 5}
+  }} />
+          <V.VictoryAxis dependentAxis tickFormat={(x) => (`${x}`)} />
+          <V.VictoryBar width={50} height={20} standalone={false} data={this.props.data.distribution} style={barStyle} alignment="start"
+                        animate={{duration: 2000, onLoad: { duration: 1000 }}}
+                        barRatio={0.25} />
         </V.VictoryChart>
       </div>
     )
@@ -140,15 +152,15 @@ class SingleAttributJBar extends React.Component {
 }
 class StatsPanel extends React.Component {
 
-  distribute(data, category) {
+  distribute(data, attribute) {
     var g=0;
     return data.reduce((distributor, currentData) => {
 
-      if (!currentData.hasOwnProperty(category)) { 
+      if (!currentData.hasOwnProperty(attribute)) { 
         return distributor;
       }
 
-      var value = currentData[category]
+      var value = currentData[attribute]
       var current
       if (distributor.values.has(value)) {
         current = distributor.values.get(value)
@@ -164,26 +176,32 @@ class StatsPanel extends React.Component {
       }, {values : new Map(), distribution: [] });
   }
   render() {
-    let statData0 = this.distribute(this.props.data, this.props.struct[0].category[0])
+    let statData0 = this.distribute(this.props.data, this.props.struct[0].attributes[0])
     console.info(statData0)
-    let statData1 = this.distribute(this.props.data, this.props.struct[0].category[1])
+    let statData1 = this.distribute(this.props.data, this.props.struct[0].attributes[1])
     console.info(statData1)
-    let statData2 = this.distribute(this.props.data, this.props.struct[0].category[2])
+    let statData2 = this.distribute(this.props.data, this.props.struct[0].attributes[2])
     console.info(statData2)
+
+    let statDataB0 = this.distribute(this.props.data, this.props.struct[1].attributes[0])
+    console.info(statDataB0)
+    let statDataB1 = this.distribute(this.props.data, this.props.struct[1].attributes[1])
+    console.info(statDataB1)
+    let statDataB2 = this.distribute(this.props.data, this.props.struct[1].attributes[2])
+    console.info(statDataB2)
 
     return (
       <div className='panel'>
         <div className='panel-row' >
-          <JPie data={statData0.distribution} title={this.props.struct[0].category[0]} />
-          <JPie data={statData1.distribution} title={this.props.struct[0].category[1]} />
-          <JPie data={statData2.distribution} title={this.props.struct[0].category[2]} />
-          <JPie data={pieData_F1} title={this.props.struct[0].title + '-1'} />
-          <JPie data={pieData_F3} title={this.props.struct[0].title + '-2'} />
+          <JPie data={statData0.distribution} struct={this.props.struct[0]} title={this.props.struct[0].attributes[0]} />
+          <JPie data={statData1.distribution} struct={this.props.struct[0]} title={this.props.struct[0].attributes[1]} />
+          <JPie data={statData2.distribution} struct={this.props.struct[0]} title={this.props.struct[0].attributes[2]} />
+          <JPie data={pieData_F1} struct={this.props.struct[0]} title={'1'} />
+          <JPie data={pieData_F3} struct={this.props.struct[0]} title={'2'} />
 
-          
-          <SingleAttributJBar data={statData0.distribution} title={this.props.struct[0].category[0]} />
-          <SingleAttributJBar data={statData1.distribution} title={this.props.struct[0].category[1]} />
-          <SingleAttributJBar data={statData2.distribution} title={this.props.struct[0].category[2]} />
+          <SingleAttributeJBar data={statDataB0} struct={this.props.struct[1]} title={this.props.struct[1].attributes[0]} />
+          <SingleAttributeJBar data={statDataB1} struct={this.props.struct[1]} title={this.props.struct[1].attributes[1]} />
+          <SingleAttributeJBar data={statDataB2} struct={this.props.struct[1]} title={this.props.struct[1].attributes[2]} />
 
           <JBar data={[barData_1, barData_2, barData_3]} title={this.props.struct[1].title} />
           <JBar2 data={[barData_1, barData_2, barData_3]} struct={this.props.struct[1]} />
