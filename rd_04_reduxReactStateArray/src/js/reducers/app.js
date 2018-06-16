dfffe8909a4b4b29cc66f43d1de7da875e81cd74
LAPTOP-135QB8F1\Alberto <petazzi63@gmail.com>
2018-06-16 16:15:17 +0200

    rd_04: made initial state as a result of a function call with 2 params

--- rd_04_reduxReactStateArray/src/js/reducers/app.js
+++ rd_04_reduxReactStateArray/src/js/reducers/app.js
@@ -1,10 +1,18 @@
-const app = (state = [
-  { id: 0, initial: 0, discriminator: 1 },
-  { id: 1, initial: 1, discriminator: 0 },
-  { id: 2, initial: 2, discriminator: 1 },
-  { id: 3, initial: 0, discriminator: 0 },
-  { id: 4, initial: 1, discriminator: 1 },
-], action) => {
+const data = [
+  { f0: 0, f1: 1, f2: 2, f3: 1, f4: 2, },
+]
+
+const infos = [
+  { initial: 'f0', discriminator: 1 },
+  { initial: 'f1', discriminator: 0 },
+  { initial: 'f2', discriminator: 1 },
+  { initial: 'f3', discriminator: 0 },
+  { initial: 'f4', discriminator: 1 },
+]
+
+const mapper = (data, infos) => infos.map((info, index) => ({ ...info, id: index, initial: data[0][info.initial] }))
+
+const app = (state = mapper(data, infos), action) => {
   switch (action.type) {
     case 'INC_NUM':
       return state.map(f => f.discriminator === 0 ? { ...f, initial: f.initial + action.payload } : f)
