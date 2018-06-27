5eb0422724fa8a6ac0fc81227245594d64c56131
apetazzi <petazzi63@gmail.com>
2018-06-27 18:02:12 +0200

    rt_07: minor change

--- rt_07_JCharts/src/actions/index.js
+++ rt_07_JCharts/src/actions/index.js
@@ -1,6 +1,6 @@
 // src/actions/index.js
 export const normalize = data => ({ type: "NORMALIZE", payload: data })
-export const load = (data, structs) => ({ type: "LOAD", payload: {data, structs}})
+export const load = (data, structs) => ({ type: "LOAD", payload: {data, structs} })
 export const reload = data => ({ type: "RELOAD", payload: data })
 export const add = data => ({ type: "ADD", payload: data })
 export const remove = data => ({ type: "REMOVE", payload: data })
