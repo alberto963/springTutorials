b28c4dc876e96e650706b317bc3f3c311a33e923
apetazzi <petazzi63@gmail.com>
2018-06-28 11:03:18 +0200

    rt_07: minor

--- rt_07_JCharts/src/components/SingleAttributeJPie.jsx
+++ rt_07_JCharts/src/components/SingleAttributeJPie.jsx
@@ -11,7 +11,7 @@ const SingleAttributeJPie = (props) => {
   return <div className='panel-elem'>
     <svg values='' width={200} height={200}>
       <V.VictoryPie standalone={false} width={200} height={200} data={props.data.distribution} innerRadius={25} labelRadius={50}
-        style={pieStyle} colorScale={['navy', 'orange', 'gold', 'cyan', 'tomato']}
+        style={pieStyle} colorScale={['green', 'navy', 'orange', 'gold', 'cyan', 'tomato']}
         animate={{ duration: 2000, onLoad: { duration: 1000 } }} />
       <V.VictoryLabel textAnchor='middle' style={{ fontSize: 10, fill: 'white' }} x={100} y={100} text={props.struct.title + '-' + props.struct.xtitle} />
     </svg>
