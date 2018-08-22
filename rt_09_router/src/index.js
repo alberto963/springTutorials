a7e0ce5af45ebfbfd7d3d79ba1a278bc442dd6dd
apetazzi <petazzi63@gmail.com>
2018-08-22 16:21:59 +0200

    rt_09: commenting

--- rt_09_router/src/index.js
+++ rt_09_router/src/index.js
@@ -1,9 +1,15 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from "react"
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link } from "react-router-dom"
+
+// BrowserRouter is the router implementation for HTML5 browsers (vs Native).
+// Link is your replacement for anchor tags.
+// Route is the conditionally shown component based on matching a path to a URL.
+// Switch returns only the first matching route rather than all matching routes.
 
 const BasicExample = () => (
-  <Router>
+
+  <BrowserRouter>
     <div>
       <ul>
         <li>
@@ -15,28 +21,32 @@ const BasicExample = () => (
         <li>
           <Link to="/topics">Topics</Link>
         </li>
+        <li>
+          <Link to="/example.json">Example.json</Link>
+        </li>
       </ul>
 
       <hr />
-
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/topics" component={Topics} />
+      <Route path="/example.json" />
+
     </div>
-  </Router>
-);
+  </BrowserRouter>
+)
 
 const Home = () => (
   <div>
     <h2>Home</h2>
   </div>
-);
+)
 
 const About = () => (
   <div>
     <h2>About</h2>
   </div>
-);
+)
 
 const Topics = ({ match }) => (
   <div>
@@ -54,21 +64,18 @@ const Topics = ({ match }) => (
     </ul>
 
     <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
+    <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>} />
+
   </div>
-);
+)
 
 const Topic = ({ match }) => (
   <div>
-    <h3>{match.params.topicId}</h3>
+    <h3>Topic: {match.params.topicId}</h3>
   </div>
-);
+)
 
 ReactDOM.render(
   <BasicExample />,
   document.getElementById('root')
-);
+)
