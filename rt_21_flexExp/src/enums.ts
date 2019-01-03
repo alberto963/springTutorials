f96cd5d3e6d278fc02ee81d65b08936a9002a0bd
apetazzi <petazzi63@gmail.com>
2019-01-03 16:27:40 +0100

    rt_21: defining project

---
+++ rt_21_flexExp/src/enums.ts
@@ -0,0 +1,11 @@
+export class ChartType {
+  public static readonly PIE = 'pie'
+  public static readonly BAR = 'hbar'
+  public static readonly GAU = 'gau'
+}
+
+export const chartTypes = [
+  'pie',
+  'hbar',
+  'gau',
+]
