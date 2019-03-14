9a98d9bf4ff6f98b8887191480ab3de8c0a5463d
apetazzi <petazzi63@gmail.com>
2019-03-14 15:09:37 +0100

    rd_07: updates cosmetic

--- rd_07_sagaTutorial/Counter.js
+++ rd_07_sagaTutorial/Counter.js
@@ -2,23 +2,23 @@
 import React, { Component, PropTypes } from 'react'
 
 const Counter = ({ value, onIncrementAsync, onIncrement, onDecrement }) =>
-      <div>
-        <button onClick={onIncrementAsync}>
-          Increment after 1 second
-        </button>
-        {' '}
-        <button onClick={onIncrement}>
-          Increment
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          Decrement
-        </button>
-        <hr />
-        <div>
-          Clicked: {value} times
-        </div>
-      </div>
+  <div>
+    <button onClick={onIncrementAsync}>
+      Increment after 1 second
+    </button>
+    {' '}
+    <button onClick={onIncrement}>
+      Increment
+    </button>
+    {' '}
+    <button onClick={onDecrement}>
+      Decrement
+    </button>
+    <hr />
+    <div>
+      Clicked: {value} times
+    </div>
+  </div>
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
