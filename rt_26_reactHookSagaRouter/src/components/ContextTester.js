5888483648ab1d0716e954cc0e8421ae3de44cce
apetazzi <petazzi63@gmail.com>
2019-05-31 17:17:43 +0200

    rt_26: chg

--- rt_26_reactHookSagaRouter/src/components/ContextTester.js
+++ rt_26_reactHookSagaRouter/src/components/ContextTester.js
@@ -10,6 +10,76 @@ import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux'
 import { getDataP2 } from '../actions'
 
+const Context = React.createContext({classes: {}, json: {}, lastId: 0, getData: null})
+
+const Card = ({classes, json}) =>  {
+
+  const {paper} = classes
+
+  return (
+    <Paper className={paper}>
+      <Grid container wrap='nowrap' spacing={2}>
+        <Grid item>
+          <Avatar>{json.id ? json.id : -9999}</Avatar>
+        </Grid>
+        <Grid item xs zeroMinWidth>
+          <Typography noWrap>{json.title}</Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  )
+}
+
+const Controls = ({classes, lastId, getData}) => {
+
+  const [id, setId] = useState(lastId)
+
+  useEffect(() => {
+    document.title = `id ${id}`
+    getData(id)
+  }, [id]) 
+
+  const {paper, margin} = classes
+
+  return (
+    <Grid item xs={12}>
+      <Grid  className={paper} container>
+        <Grid item>
+        <Button variant='outlined' size='medium' color='primary' className={margin} onClick={useCallback(() => setId(id - 1), [id])}>
+          Previous
+        </Button>
+        <Button variant='outlined' size='medium' color='primary' className={margin} onClick={useCallback(() => setId(id + 1), [id])}>
+          Next
+        </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+const ContextTester = (props) => {
+
+  const {classes} = props
+  
+  return (
+    <div className={classes.root}>
+      <Card {...props} />
+      <Controls {...props} />
+    </div>
+  )
+}
+
+const mapStateToProps = ({options, data}) => (
+  {
+    lastId: options.idP2,
+    json: data.jsonP2
+  }
+)
+
+const mapDispatchToProps = {
+  getData: getDataP2,
+}
+
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -25,54 +95,6 @@ const styles = theme => ({
     margin: theme.spacing(1),
   },
 })
-
-const ContextTester = ({classes, getData, json, lastId}) => {
-
-  const [id, setId] = useState(lastId)
-
-  useEffect(() => {
-    document.title = `id ${id}`
-    getData(id)
-  }, [id])  
-  
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container wrap='nowrap' spacing={2}>
-          <Grid item>
-            <Avatar>{json.id ? json.id : id}</Avatar>
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Typography noWrap>{json.title}</Typography>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Grid item xs={12}>
-        <Grid  className={classes.paper} container>
-          <Grid item>
-          <Button variant='outlined' size='medium' color='primary' className={classes.margin} onClick={useCallback(() => setId(id - 1), [id])}>
-            Previous
-          </Button>
-          <Button variant='outlined' size='medium' color='primary' className={classes.margin} onClick={useCallback(() => setId(id + 1), [id])}>
-            Next
-          </Button>
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
-  )
-}
-
-const mapStateToProps = ({options, data}) => (
-  {
-    lastId: options.idP2,
-    json: data.jsonP2
-  }
-)
-
-const mapDispatchToProps = {
-  getData: getDataP2,
-}
 
 // export default withStyles(styles)(ContextTester)
 // export default connect(mapStateToProps, mapDispatchToProps)(styledContextTester)
