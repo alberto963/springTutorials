deaac0d822b5e25f2bf09c49dea140d03ae2a3dc
apetazzi <petazzi63@gmail.com>
2019-05-31 16:05:59 +0200

    rt_28: chg

--- rt_28_reactContext/src/app.js
+++ rt_28_reactContext/src/app.js
@@ -1,21 +1,45 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useCallback } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const BtnColorContext = React.createContext('btn btn-dark')
+const init = 'btn btn-dark'
+const BtnColorContext = React.createContext(init)
 
-const App = () =>
-  <div>
-    <Button />
-    <BtnColorContext.Provider value="btn btn-info">
-      <Button />
-    </BtnColorContext.Provider>
-    <BtnColorContext.Provider value="btn btn-warning">
-      <Button />
-    </BtnColorContext.Provider>
-    <BtnColorContext.Consumer>
-      {value => <Button className={value} />}
-    </BtnColorContext.Consumer>
-  </div>
+const App = () => {
+  const [c, setC] = useState(init)
+
+  const toggleC = (c === init ? 'btn btn-success' : init)
+
+  return (
+    <div>
+      <button className={'btn btn-lg'} onClick={useCallback(() => setC(toggleC), [c])}>>
+        Change context
+      </button>
+      <div>
+        <Button />
+        <BtnColorContext.Provider value="btn btn-info">
+          <Button />
+        </BtnColorContext.Provider>
+        <BtnColorContext.Provider value="btn btn-warning">
+          <Button />
+        </BtnColorContext.Provider>
+
+        <BtnColorContext.Provider value={c}>
+          <Button />
+        </BtnColorContext.Provider>
+
+        <BtnColorContext.Provider value={c}>
+          <BtnColorContext.Consumer>
+            {value => console.info(value)}
+          </BtnColorContext.Consumer>
+        </BtnColorContext.Provider>
+
+        <BtnColorContext.Provider value={c}>
+          <ConsumerButton />
+        </BtnColorContext.Provider>
+      </div>
+    </div>
+  )
+}
 
 const Button = props => // Note: props are not used
   <div className="container">
@@ -27,4 +51,11 @@ const ThemedButton = props => // Note: props are not used
     hello
   </button>
 
+const ConsumerButton = props =>
+  <div className="container">
+    <BtnColorContext.Consumer>
+      {(value) => <button style={{margin: '10px'}} className={value} onClick={() => {}}>Consumer</button>}
+    </BtnColorContext.Consumer>
+  </div>
+  
 export default App
