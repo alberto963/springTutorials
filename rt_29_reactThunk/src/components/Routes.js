2505fbba02eadf48297f27cf143bb45864bf2e25
apetazzi <petazzi63@gmail.com>
2019-06-12 09:07:39 +0200

    rt_29: chg

--- rt_29_reactThunk/src/components/Routes.js
+++ rt_29_reactThunk/src/components/Routes.js
@@ -1,32 +1,49 @@
 import React from 'react'
 
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+
 import Todos from './Todos'
 import Home from './Home'
 import About from './About'
 import NoMatch from './NoMatch'
 
-const Routes = () => 
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/todos">jsonplaceholder todos test client</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/todos" component={Todos} />
-        <Route path="/about" component={About} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}))
+
+const Routes = () => {
+  const classes = useStyles()
+
+  return (
+    <Typography component='div' style={{ padding: 8 * 3 }}>
+      <Router>
+      <div className={classes.root}>
+        <AppBar position='static'>
+          <Tabs variant='fullWidth' value={'/'}>
+            <Tab label='Home' component={Link} to='/' />
+            <Tab label='Todos' component={Link} to='/todos' />
+            <Tab label='About' component={Link} to='/about' />
+          </Tabs>
+        </AppBar>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/todos' component={Todos} />
+          <Route path='/about' component={About} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+      </Router>
+    </Typography>
+  )
+}
 
 export default Routes
