045431ab6e1980bc862c189bd414181fc78fe87c
apetazzi <petazzi63@gmail.com>
2019-08-23 16:34:28 +0200

    rt_30: chg

--- rt_30_reactVis/src/XYPlot.js
+++ rt_30_reactVis/src/XYPlot.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from './Button'
 
@@ -13,66 +13,71 @@ import {
   MarkSeries
 } from 'react-vis'
 
-function generateData() {
-  return [...new Array(10)].map(row => ({
+const generateData = () => [...new Array(10)].map(r => ({
     x: Math.random() * 5,
     y: Math.random() * 10
   }))
-}
-
+ 
 const MODE = ['noWobble', 'gentle', 'wobbly', 'stiff']
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(2),
   },
+  box: {
+    width: '310px', 
+    height: '310px',
+    borderColor: 'grey',
+  }
 }))
-
-const defaultProps = {
-  bgcolor: 'background.paper',
-  style: { width: '310px', height: '310px' },
-  borderColor: 'text.primary',
-}
 
 const MyXYPlot = props => {
 
-  const [state, setState] = React.useState({
+  const [state, setState] = useState({
     data: generateData(),
-    modeIndex: 0,
-    classes: useStyles()
+    loading: false,
+    modeIndex: 0
   })
 
-  const updateModeIndex = React.useCallback(increment => () => {
-    const newIndex = state.modeIndex + (increment ? 1 : -1)
-    const modeIndex = newIndex < 0 ? MODE.length - 1 : newIndex >= MODE.length ? 0 : newIndex
+  const updateModeIndex = useCallback(increment => () => {
+    const newIndex = state.modeIndex + increment 
+    const modeIndex = newIndex < 0 ? MODE.length - 1 : newIndex % MODE.length
     setState({ ...state, modeIndex })
   }, [state])
 
-  const {modeIndex, data, classes} = state
+  const updatedata = useCallback(() => {
+    setState({...state, loading: true})
+    setTimeout(() => setState({...state, loading: false, data: generateData()}), 1000)
+  }, [state])
+
+  const {modeIndex, loading, data} = state
+
+  const classes = useStyles()
 
   return <div className='centered-and-flexed'>
       <div className='centered-and-flexed-controls'>
         <Button className={classes.button}
-          onClick={updateModeIndex(false)}
+          onClick={updateModeIndex(-1)}
           buttonContent={'PREV'}
         />
         <div> {`ANIMATION TECHNIQUE: ${MODE[modeIndex]}`} </div>
         <Button className={classes.button}
-          onClick={updateModeIndex(true)}
+          onClick={updateModeIndex(+1)}
           buttonContent={'NEXT'}
         />
       </div>
-      <Box border={1} {...defaultProps}>
-      <XYPlot width={300} height={300}>
-        <VerticalGridLines />
-        <HorizontalGridLines />
-        <XAxis />
-        <YAxis />
-        <MarkSeries animation={MODE[modeIndex]} data={data} />
-      </XYPlot>
+      <Box className={classes.box} border={1} bgcolor='primary.main'>
+        {loading && <div className={classes.button}>Loading...</div>}
+        {!loading && <XYPlot width={300} height={300}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <MarkSeries animation={MODE[modeIndex]} data={data} />
+        </XYPlot>}
       </Box>
       <Button className={classes.button}
-        onClick={() => setState({...state, data: generateData()})}
+        onClick={updatedata}
         buttonContent={'UPDATE DATA'}
       />
     </div>
