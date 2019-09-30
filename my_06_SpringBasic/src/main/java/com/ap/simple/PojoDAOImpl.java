c9528d23abae118f47f9e8002b8f6534a2039c0d
apetazzi <petazzi63@gmail.com>
2019-09-30 16:18:57 +0200

    my_06: chg

--- my_06_SpringBasic/src/main/java/com/ap/simple/PojoDAOImpl.java
+++ my_06_SpringBasic/src/main/java/com/ap/simple/PojoDAOImpl.java
@@ -5,10 +5,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+@Component
 @Repository
 public class PojoDAOImpl implements PojoDAO {
+	
 	@Autowired
 	private SessionFactory factory;
 
