4f08a9b492332df450529f3a69a8f45a97ad1bca
apetazzi <petazzi63@gmail.com>
2019-10-03 07:57:07 +0200

    my_09: chg

--- my_09_HibernateBasic/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ my_09_HibernateBasic/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -7,11 +7,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 
+ * @SpringBootApplication is a convenience annotation that adds all of the
+ *                        following:
+ * 
+ * @Configuration: Tags the class as a source of bean definitions for the
+ *                 application context.
+ * 
+ * @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on
+ *                           classpath settings, other beans, and various
+ *                           property settings. For example, if spring-webmvc is
+ *                           on the classpath, this annotation flags the
+ *                           application as a web application and activates key
+ *                           behaviors, such as setting up a DispatcherServlet.
+ * 
+ * @ComponentScan: Tells Spring to look for other components, configurations,
+ *                 and services in the com/example package, letting it find the
+ *                 controllers.
+ * 
+ */
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
 
+	/**
+	 * The AccessingDataJpaApplication class includes a main() method that puts the
+	 * CustomerRepository through a few tests. First, it fetches the
+	 * CustomerRepository from the Spring application context.
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataJpaApplication.class);
 	}
@@ -53,5 +79,4 @@ public class AccessingDataJpaApplication {
 			log.info("");
 		};
 	}
-
 }
--- my_09_HibernateBasic/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ my_09_HibernateBasic/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * 
+ * This is what makes Spring Data JPA so powerful: You need not write an
+ * implementation of the repository interface. Spring Data JPA creates an
+ * implementation when you run the application.
+ * 
+ */
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
 	List<Customer> findByLastName(String lastName);
