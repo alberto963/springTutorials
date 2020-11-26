f46ee01a30b94b31bebe9c5681e7a48384a846c1
apetazzi <alberto.petazzi.ext@nokia.com>
2020-11-26 16:47:57 +0100

    my_10: chg

--- my_10_SpringDoc/src/main/java/spring/tutorial/Example.java
+++ my_10_SpringDoc/src/main/java/spring/tutorial/Example.java
@@ -10,9 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import spring.tutorial.services.DatabaseAccountService;
 
@@ -33,15 +33,15 @@ public class Example {
 		return "Hello World!! Account open " + das.open() + " at " + new Date();
 	}
 
-//	@RequestMapping("/load")
-//	public String load() {
-//		return "index.html";
-//	}
-	
-	@RequestMapping(value = "/load")
-	public ModelAndView showForm() {
-		return new ModelAndView();
+	@RequestMapping("/load")
+	public String load(Model model) {
+		return "index";
 	}
+	
+//	@RequestMapping(value = "/load")
+//	public ModelAndView showForm() {
+//		return new ModelAndView();
+//	}
 
 	@RequestMapping("/exit")
 	public String exit() {
