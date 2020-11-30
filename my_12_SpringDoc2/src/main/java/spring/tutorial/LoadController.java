4669223d35fb05aa68d10b844cbb7f1333d3b6d4
apetazzi <alberto.petazzi.ext@nokia.com>
2020-11-30 15:46:55 +0100

    my_12: chg

--- my_12_SpringDoc2/src/main/java/spring/tutorial/LoadController.java
+++ my_12_SpringDoc2/src/main/java/spring/tutorial/LoadController.java
@@ -3,12 +3,13 @@ package spring.tutorial;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class LoadController {
 
 	@RequestMapping("/load")
-	public String load(Model model) {
+	public String load(WebRequest request, Model model) {
 		model.addAttribute("status", "Changed default templates dir to dist");
 		model.addAttribute("code", "with success");
 		return "load";
