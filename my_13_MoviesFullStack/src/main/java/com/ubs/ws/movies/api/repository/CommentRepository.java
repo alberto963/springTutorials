d57abc8597a85b8a6ebe27b3456c418988386aea
Alberto <petazzi63@gmail.com>
2020-11-30 17:50:15 +0100

    my_13: del

--- my_13_MoviesFullStack/src/main/java/com/ubs/ws/movies/api/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ubs.ws.movies.api.repository;
-
-import com.ubs.ws.movies.pojo.bean.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-}
--- my_13_MoviesFullStack/src/main/java/com/ubs/ws/movies/api/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ubs.ws.movies.api.repository;
-
-import com.ubs.ws.movies.pojo.bean.Movie;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovieRepository extends CrudRepository<Movie, Long> {
-}
