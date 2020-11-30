29467d253e5771262a11f1201c93bf950fec077d
apetazzi <alberto.petazzi.ext@nokia.com>
2020-11-30 15:06:54 +0100

    my_12: chg

--- my_12_SpringDoc2/src/main/java/spring/tutorial/LoadController.java
+++ my_12_SpringDoc2/src/main/java/spring/tutorial/LoadController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoadController {
-	
+
 	@RequestMapping("/load")
 	public String load(Model model) {
+		model.addAttribute("status", "Changed default templates dir to dist");
+		model.addAttribute("code", "with success");
 		return "load";
 	}
-	
+
 }
