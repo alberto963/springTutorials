b769fbf1ad1df868e580dd1aaecc87af101b3c56
Alberto <petazzi63@gmail.com>
2020-11-30 12:46:23 +0100

    my_12: chg

---
+++ my_12_SpringDoc2/src/main/java/spring/tutorial/WebConfig.java
@@ -0,0 +1,37 @@
+package spring.tutorial;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/load").setViewName("index.html");
+//	}
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		WebMvcConfigurer.super.addResourceHandlers(registry);
+//		String pathPatterns = "dist/**";
+//		registry.addResourceHandler(pathPatterns).addResourceLocations("/" + pathPatterns);
+//	}
+
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+//
+//		// bean.setViewClass(JstlView.class);
+//		bean.setPrefix("/dist/");
+//		bean.setSuffix(".html");
+//		bean.setOrder(2);
+//
+//		return bean;
+//	}
+}
